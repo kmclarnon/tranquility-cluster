40414687025500c7be19ae2c30461a6aa8bfc278
Kevin McLarnon <mclarnon.k@gmail.com>
2019-09-12 17:06:56 -0400

    install metallib config map

--- setup-scripts/install-metallb.sh
+++ setup-scripts/install-metallb.sh
@@ -2,6 +2,10 @@
 
 set -e
 
-# Install MetalLb
+# Install metallb
+echo "Installing metallb"
 kubectl apply -f https://raw.githubusercontent.com/google/metallb/v0.8.1/manifests/metallb.yaml
 
+# Install the config for metallb
+echo "Configuring metallb in layer2 mode for 192.168.2.1 - 192.168.2.255"
+kubectl apply -f ../custom-objects/metallb-config-map.yaml
