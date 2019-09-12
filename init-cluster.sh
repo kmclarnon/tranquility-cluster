c345d91f6d451f2d4f9af0491be268115a987adb
Kevin McLarnon <mclarnon.k@gmail.com>
2019-09-11 21:37:27 -0400

    complete v1 of init

--- init-cluster.sh
+++ init-cluster.sh
@@ -2,10 +2,44 @@
 
 set -e
 
-# Execute setup on master host
+declare NODEARRAY
+
+# Get our configuration
 read -p "Enter master host: " MASTER
+read -p "How many worker nodes do you wish to set up: " NODECOUNT
+
+for i in $(seq 1 $NODECOUNT)
+do
+  read -p "Enter worker host: " TMP
+  NODEARRAY[i]=$TMP
+done
+
+# Execute setup of our master node
+echo "Executing setup on master node: $MASTER"
 ssh root@$MASTER ARG1="1" ARG2="$MASTER" 'bash -s' < ./setup-scripts/init-node.sh
 
 # capture our join command
 JOIN_CMD=$(ssh root@$MASTER 'bash -s' < ./setup-scripts/print-token.sh)
-echo $JOIN_CMD
+
+# setup this computer for access to the cluster via kubectl
+mkdir -p $HOME/.kube
+scp root@$MASTER:/etc/kubernetes/admin.conf $HOME/.kube/config
+chown $(id -u):$(id -g) $HOME/.kube/config
+
+# Execute setup on node
+for i in "${NODEARRAY[@]}"
+do
+  echo "Executing setup on worker node: $i"
+  ssh root@$i ARG1="0" ARG2="$i" 'bash -s' < ./setup-scripts/init-node.sh
+  ssh root@$i "$JOIN_CMD"
+done
+
+echo "*************************************"
+echo "*                                   *"
+echo "*  Cluster Initialization Complete  *"
+echo "*                                   *"
+echo "*************************************"
+
+kubectl get nodes
+
+
---
+++ setup-scripts/reset.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e
+
+kubeadm reset -f
+apt-get purge --yes kubeadm kubectl kubelet kubernetes-cni kube*
+apt-get autoremove --yes
+rm -rf ~/.kube
